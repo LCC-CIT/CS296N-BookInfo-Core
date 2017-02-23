8d1c2bf0d540420f9ea3de3453147bb89bbe4741
Student <Student@1912000-C18077>
2017-02-23 11:56:00 -0800

    Added controller and view for entering a review... still in process

---
+++ src/BookInfo/Controllers/ReviewController.cs
@@ -0,0 +1,37 @@
+﻿using BookInfo.Models;
+using BookInfo.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookInfo.Controllers
+{
+    public class ReviewController : Controller
+    {
+        private IBookRepository bookRepo;
+
+        public ReviewController(IBookRepository repo)
+        {
+            bookRepo = repo;
+        }
+
+        [HttpGet]
+        public ViewResult ReviewForm(string title, int id)
+        {
+            ViewBag.BookTitle = title;
+            ViewBag.Id = id;
+            // TODO Create a view model
+
+            return View(new Review());
+        }
+
+        [HttpPost]
+        public ViewResult ReviewForm(int rating, string body)
+        {
+            return null;
+        }
+
+    }
+}
