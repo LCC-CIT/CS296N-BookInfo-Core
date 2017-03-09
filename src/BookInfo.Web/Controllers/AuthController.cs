57433053a3987548f7277b482d2295b3aa86f80d
profbird <birdb@lanecc.edu>
2017-03-08 17:56:30 -0800

    Added basic infrastructure. No controller methods yet.

---
+++ src/BookInfo.Web/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BookInfo.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BookInfo.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Register()
+        {
+            return View(new AuthViewModel() );
+        }
+
+        public IActionResult Login()
+        {
+            return View(new AuthViewModel());
+        }
+    }
+}
---
+++ src/BookInfo.Web/Models/AuthViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookInfo.Models
+{
+    public class AuthViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
