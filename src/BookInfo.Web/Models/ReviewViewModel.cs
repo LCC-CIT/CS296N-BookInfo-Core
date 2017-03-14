17c3c61dd9b4785adfc25ace640a9a8cc108bd91
profbird <birdb@lanecc.edu>
2017-03-14 07:31:56 -0700

    Added validation data annotations

--- src/BookInfo.Web/Models/ReviewViewModel.cs
+++ src/BookInfo.Web/Models/ReviewViewModel.cs
@@ -6,7 +6,15 @@ namespace BookInfo.Models
     {
         public int BookId { get; set; }
         public string Title { get; set; }
-        public string Body {get; set;}
+
+        [Required]
+        [MinLength(10, ErrorMessage = "You must enter at least 10 characters")]
+        [Display(Name = "Review Text")]
+        public string Body { get; set; }
+
+        [Required]
+        [Range(1.0, 5.0, ErrorMessage = "Please enter a number from 1 to 5")]
+        [Display(Name = "Book Rating")]
         public int Rating { get; set; }
     }
 }
