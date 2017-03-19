d4280c3852f56f5adddb5ed53f03a1a388d63701
profbird <birdb@lanecc.edu>
2017-03-19 11:22:47 -0700

    Added more comments

--- src/BookInfo.Web/Controllers/AuthController.cs
+++ src/BookInfo.Web/Controllers/AuthController.cs
@@ -96,14 +96,26 @@ namespace BookInfo.Web.Controllers
 
 /* Notes:
 
-1. There is not a non-async (synchronous) version of UserManager.CreateAsync
-  
-2. Task: The Task class represents a single operation that does not return a value 
+1. There are not non-async (synchronous) versions of the UserManager and SignInManager async methods.
+   If we were using MVC 5 or 6 (not Core MVC), we could have made our code easier to understand by using
+   the non-async versions of the async methods. But since Core MVC is still so new the non-async versions 
+   haven't been written yet-- they wanted to give us the higher performance options first. 
+   So we have to use async methods and we have to put them in our own async method that returns a Task<> object.
+   You can read about async methods here: https://msdn.microsoft.com/en-us/library/mt674882.asp
+
+2. In the HttpPost Register method, look at the header. Note that it is an async method, 
+    meaning that it runs on a separate thread and when it's finished, it returns a Task<IActionResult> object.
+    Within the Register method, in a statement where I call an async method, like this one:
+        IdentityResult result = await userManager.CreateAsync(user, vm.Password);
+    I use the await keyword so that execution will pause until the CreateAsync method, 
+    which is running on a separate thread, finishes.
+
+3. Task<>: The Task class represents a single operation that does not return a value 
    and that usually executes asynchronously. Task objects are one of the central components 
    of the task-based asynchronous pattern first introduced in the .NET Framework 4. 
    Because the work performed by a Task object typically executes asynchronously on a thread pool 
    thread rather than synchronously on the main application thread, you can use the Status property, 
    as well as the IsCanceled, IsCompleted, and IsFaulted properties, to determine the state of a task. 
    From: https://msdn.microsoft.com/en-us/library/system.threading.tasks.task(v=vs.110).aspx
- 
+
  */
